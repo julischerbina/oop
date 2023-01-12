050740c3f9331489bd7f7eed09e9b1ee97406482
jul <julia_sky@mail.ru>
2023-01-12 20:25:39 +0300

    Урок ООП. Домашняя работа.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,24 @@
-public class Main {
+import transport.Automobile;
 
-    public static Automobile[] automobiles = {
-            new Automobile("","Granta", 1.7, "желтый", 2015, "Россия"),
-            new Automobile("Audi", "A8 50 L TDI quattro", 3, "черный", 2020, "Германия"),
-            new Automobile("BMW", "", -1, "черный", -5, "Германия"),
-            new Automobile("Kia", "Sportage 4-го поколения", -0.5, "красный", 2018, ""),
-            new Automobile("Hyundai", "Avante", 1.6, "оранжевый", 2016, "Южная Корея")
-    };
+public class Main {
     public static void main(String[] args) {
-        for (Automobile car: automobiles) {
-            System.out.println(car);
-        }
+        Automobile.Key universalKey = new Automobile.Key(true, true);
+        Automobile lada = new Automobile("", "Granta", 1.7, "желтый", 2015, "Россия",
+                "механическая", "седан", "ф555фф88", 4,
+                2, new Automobile.Key(true, false));
+        Automobile audi = new Automobile("Audi", "A8 50 L TDI quattro", 3, "черный",
+                2020, "Германия", "автомат", "седан", "г564гг85", 5,
+                6, universalKey);
+        Automobile bmw = new Automobile("BMW", "", -1, "черный", -5, "Германия", "автомат",
+                "седан", "в555вв65", 5, 5, universalKey);
+        Automobile kia = new Automobile("Kia", "Sportage 4-го поколения", -0.5, "красный", 2018, "",
+                "механическая", "седан", "а555аа55", 4, 2, universalKey);
+        Automobile hyundai = new Automobile("Hyundai", "Avante", 1.6, "оранжевый", 2016, "Южная Корея",
+                "механическая", "седан", "в555вв65", 5, 5, universalKey);
+        System.out.println(lada);
+        System.out.println(audi);
+        System.out.println(bmw);
+        System.out.println(kia);
+        System.out.println(hyundai);
     }
 }
